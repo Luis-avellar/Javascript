84c2fefeddeea367cfa48ff35eeb382ac9914436
Luis-avellar <avellar.mr@gmail.com>
2022-09-28 13:20:30 -0300

    ex 07 falta terminar

---
+++ ex07/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img= window.document.getElementById('imagem')
+    var data = new Date()
+    var agora = data.getHours()
+
+    msg.innerHTML = `Agora são ${agora} horas.`
+    if(agora>=0 && agora<12){
+        img.src = 'images/foto-manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if(agora>=12 && agora<=18){
+        img.src = 'images/foto-tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'images/foto-noite.png'
+        document.body.style.background = '#515154'
+    }
+
+}
